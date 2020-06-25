2584ab440f0eb96d89cae18520c7c5edd61d2fd5
Rafael Fonseca <rafael_010697@hotmail.com>
2020-06-25 12:35:02 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function calcular(tipo, valor){
-    
+    if(tipo === 'acao'){
+        if(valor === 'clear'){
+            // limpar a tela (resultado)
+            document.getElementById('resultado').value = ''
+        }
+        if(valor === '+' || valor === '-' || valor === '/' || valor === '*' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+        if(valor === '='){
+            var valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo
+        }
+    }else if(tipo === 'valor'){
+        document.getElementById('resultado').value += valor
+    }
 }
